6799340c64b1b47e89815746dd31eb0ae3582f33
Dmitry_Zhilenkov <zhildemon@gmail.com>
2019-03-31 23:00:53 +1000

    simple gen

--- gen.py
+++ gen.py
@@ -1,9 +1,31 @@
-# Coming soon...
+# Простой вариант http://www.pymix.org/pymix/index.php?n=PyMix.Tutorial#quickstart
 
+# Чуть более полезный
 import pandas as pd
 import numpy as np
 
+# Fun
+def genpoints(listofdistr, numobj, cump):
+    lst = []
+    for i in range(numobj):
+        j = np.argmax(np.array(cump) > np.random.uniform())
+        r = eval('np.random.' + listofdistr[j-1][0] + '(' + str(listofdistr[j-1][1]) + ',' + str(listofdistr[j-1][2]) + ')')
+        lst.append(r)
+    return lst
+
+# Блок определения входных данных
 numobj = 10
 listofdistr = [['normal', 0, 1], ['uniform', 0, 1]]
 
-print(eval('np.random.' + listofdistr[0][0] + '(' + str(listofdistr[0][1]) + ',' + str(listofdistr[0][2]) + ')'))
+# Фиксируем генератор случайных чисел
+#np.random.seed(123)
+
+# Генерация плотности распределения для списка распределений
+p = np.random.uniform(size=len(listofdistr))
+p = p/sum(p)
+
+# Построение дискретной функции распределения
+cump = np.cumsum(p)
+
+lst = genpoints(listofdistr, numobj, p)
+print(lst)
